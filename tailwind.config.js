8fb4cd0442ea7f84589538e31ff6b53e857e9fc2
pakinphan <thursbeta@gmail.com>
2023-10-19 22:03:43 +0700

    add illustrates

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,9 +23,9 @@ module.exports = {
             "warning": "#fbbd23",
             "error": "#f87272",
           },
-        }, "light", "dark",
+        }, "light","dracula",
       ],// true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    darkTheme: "dracula", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
