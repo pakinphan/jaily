06ed2c83fcfbf1dcafe5acf035450deace6d8248
pakinphan <thursbeta@gmail.com>
2023-10-26 23:40:26 +0700

    carousel start

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,9 +25,8 @@ module.exports = {
             "error": "#f87272",
           },
 
-        }, "light","dracula",
-      ],// true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dracula", // name of one of the included themes for dark mode
+        },"dracula",
+      ],// true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"] // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
