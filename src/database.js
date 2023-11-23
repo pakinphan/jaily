e75d17f3e65171bb0a7584794a24325d4a2f44aa
MUNA <jirapat.phantte@gmail.com>
2023-11-24 02:17:31 +0700

    update login method
    แต่ยัง. ใช้ไม่ได้.

---
+++ src/database.js
@@ -0,0 +1,12 @@
+// var mysql = require("mysql2");
+
+// const dbConnection = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "jailydb",
+// }).promise();
+
+// module.exports = dbConnection;
+
+//ยัง
