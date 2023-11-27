6b650c491ffc7e83f72f76f277485cf31d0f130b
pakinphan <thursbeta@gmail.com>
2023-11-28 00:19:10 +0700

    update navbar

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,9 +1,19 @@
 const darkModeToggle = document.getElementById("dark-mode-toggle");
 
+// Check if the user has a preferred theme in local storage
+const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+// Apply the theme on page load
+if (isDarkMode) {
+    document.documentElement.classList.add('dark');
+    darkModeToggle.checked = true;
+}
+
 darkModeToggle.addEventListener("change", () => {
-    if (darkModeToggle.checked) {
-        document.documentElement.classList.add("dark");
-    } else {
-        document.documentElement.classList.remove("dark");
-    }
+    // Toggle the class on the document element
+    document.documentElement.classList.toggle("dark");
+
+    // Update the user's preference in local storage
+    const newMode = document.documentElement.classList.contains('dark') ? 'enabled' : 'disabled';
+    localStorage.setItem('darkMode', newMode);
 });
